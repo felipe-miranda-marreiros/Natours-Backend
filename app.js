72898f42e9037784212a6482d930452e2dd14f78
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-04-25 13:34:56 -0300

    Método POST - utilizando Middleware

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const express = require('express');
+
 const app = express();
+
+//Middleware - é chamado assim porque fica entre o Request e o Response.
+app.use(express.json());
 
 //Essa função será executada apenas uma vez.
 const tours = JSON.parse(
@@ -17,6 +21,31 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //Usando método POST
+
+  //Req.body está disponível agora porque solicitamos no app.use(express.json());
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  //Usando push() para adicionar no final da Array.
+  tours.push(newTour);
+
+  //Usando writeFile() para realmente adicionar no banco de dados e ter um response.
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 //Criando servidor usando porta 3000.
