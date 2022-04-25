438c09d73eda6914af764d0a01b0dd5db44a8fd3
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-04-25 11:07:36 -0300

    Utilizando método GET - Enviando API

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+//Essa função será executada apenas uma vez.
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+//Essa função ocorrerá no Event Loop.
+app.get('/api/v1/tours', (req, res) => {
+  //Usando método GET
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint');
-});
-
+//Criando servidor usando porta 3000.
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
