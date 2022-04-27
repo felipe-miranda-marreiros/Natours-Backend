e2bec30bad468f041fbb62613b0be865c314055b
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-04-27 12:35:49 -0300

    Configurando variáveis de ambiente

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //Middlers
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 //Podemos enviar arquivos estaticos usando express.static(<caminho>)
 app.use(express.static(`${__dirname}/public/`));
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
-//Criando servidor usando porta 3000.
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
