305d7b15245e7187b9747a8e69cea4d8cb216f1a
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-05-02 21:35:02 -0300

    Implementando Filtering usando operadores de comparação com Mongoose Query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,11 +3,16 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   //Usando método GET
   try {
+    //Build Query
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    //Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr));
 
     const tours = await query;
 
