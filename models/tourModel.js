e10c9b0abbdf9613aeaef60e4d877761b99dc8bc
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-05-07 22:59:26 -0300

    Data validation - custom validators com Validator Js

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,10 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have less or equal than 40 characters'],
       minlength: [10, 'A tour must have at least 10 characters'],
+      // validate: [
+      //   validator.isAlpha,
+      //   'Tour name must be only contain characters',
+      // ],
     },
     slug: String,
     duration: {
@@ -42,7 +47,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (value) {
+          // this only points to current document on NEW documents creation
+          return value < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
