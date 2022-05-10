ef75fd93c8371cd81347717e4165f621d99bf0bb
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-05-10 15:44:17 -0300

    Erros durante Desenvolvimento e Produção

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,37 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    err: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //Operation, trusted = do AppError
+  //Enviando a mensagem para o cliente
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR', err);
+    //A mensagem abaixo não será enviada para o cliente, apenas para o desenvolvedor
+    res.status(500).json({
+      status: err,
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV) {
+    sendErrorProd(err, res);
+  }
 };
