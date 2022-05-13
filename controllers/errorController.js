47e63c26d50137c767dbc58d27c36a08938e377f
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-05-13 08:46:49 -0300

    [FIX]: middleware executando apos troca de process.env

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -49,8 +49,8 @@ module.exports = (err, req, res, next) => {
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err };
+  } else if (process.env.NODE_ENV === 'production ') {
+    let error = Object.create(err);
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateField(error);
