d4c678d8344c28a6a0e78fda50f7927f826ce1ab
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-05-21 02:05:19 -0300

    Usando Geospatial Aggregation para calcular distancias

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -122,3 +122,47 @@ exports.getTourWithin = catchAsync(async (req, res, next) => {
     }
   });
 });
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  //Exemplo: req.params = /distances/:latlng/unit/:unit
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please provide latitude and longitude in the format lat, lng.',
+        400
+      )
+    );
+  }
+
+  const distances = await Tour.aggregate([
+    {
+      //geoNear sempre tem que ser o primeiro Aggregation de todo o Model, no caso, do Tour.
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1]
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier
+      }
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1
+      }
+    }
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances
+    }
+  });
+});
