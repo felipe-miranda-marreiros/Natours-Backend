0680cd6a9deefc6c0a96ede9cb4fa3aeac5180f2
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-05-23 23:45:46 -0300

    Implementando sistema de login usando cookie-parser e parcel

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -48,6 +49,7 @@ app.use('/api', limiter);
 
 //Body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 //Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -72,7 +74,6 @@ app.use(
 //Test middlware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  //console.log(re.headers)
   next();
 });
 
