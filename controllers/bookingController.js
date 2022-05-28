df94bb9d4f5e0cf489d167c8ce6274031d8af6a5
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-05-28 20:15:09 -0300

    [FIXED]: Stripe session

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -55,15 +55,16 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 
 exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
+
   let event;
   try {
-    event = stripe.webhooks.constructorEvent(
+    event = stripe.webhooks.constructEvent(
       req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET
